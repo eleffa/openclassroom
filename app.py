978446a10c22dd3ce43c3f182e7c2c4caf65e292
Emma Effa <eleffa@yahoo.com>
2023-06-23 14:59:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import joblib
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+model_tfidf = joblib.load('tfidf_model.bkl')
+model_classification = joblib.load('tag_classification.bkl')
+tag_label = joblib.load('multilabel_binarizer.bkl')
+
+# Header of tag Prediction
+html_temp="""<div style="background-color:#F5F5F5"> <h1 style="color:#31333F;text-align:center;"> Customer Satisfaction Prediction </h1></div>"""
+
+st.text_input("Enter your text here", key="text")
+
+# You can access the value at any point with:
+text = st.session_state.text
+
+text = cleanText(text)
+input_vector = model_tfidf.transform(text)        
+res = model_classification.predict(input_vector)
+res = tag_label.inverse_transform(res)
+
+st.success(f'The tag list is {res}')
