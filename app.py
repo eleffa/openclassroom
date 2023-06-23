e407c4836fc2876d8308dcc8cdd47a08e0d8ea70
Emma Effa <eleffa@yahoo.com>
2023-06-23 20:46:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,28 @@
 import streamlit as st
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.tag.perceptron import PerceptronTagger
-from sklearn.feature_extraction.text import  TfidfVectorizer
-from bs4 import BeautifulSoup
-from collections import defaultdict
-import numpy as np
-import matplotlib.pyplot as plt
-import string
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import re
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('brown')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-lm = WordNetLemmatizer()
-lemmatizer = WordNetLemmatizer()
-wordnet_map = {"N":wordnet.NOUN, "V":wordnet.VERB, "J":wordnet.ADJ, "R":wordnet.ADV}
+import joblib
 
-
-def message_cleaning(message):
-    Test_punc_removed = [char for char in message if char not in string.punctuation]
-    Test_punc_removed_join = ''.join(Test_punc_removed)
-    #Test_punc_removed_join = ''.join([i for i in Test_punc_removed_join if not i.isdigit()]) # retirer les chiffres
-    Test_punc_removed_join_clean = [word.lower() for word in Test_punc_removed_join.split() if word.lower() not in stopwords.words('english')]
-    return Test_punc_removed_join_clean
-
-def lemmatize_words(text):
-    pos_tagged_text = nltk.pos_tag(text.split())
-    return " ".join([lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
-
-def remove_code(text):
-    pointer=text.find('<code>')
-    while pointer!=-1:
-        ender=text.find(u'</code>')
-        text=text.replace(text[pointer:ender+7],' ')
-        pointer=text.find('<code>')
-    return text
-
-def remove_html(text):
-    return BeautifulSoup(text, 'lxml').get_text()
-
-def cleanText(text):
-  text = remove_code(text)
-  text = remove_html(text)
-  text = lemmatize_words(text)
-  text = message_cleaning(text)
-  return text
-
-#import joblib
-
-#model_tfidf = joblib.load('tfidf_model.bkl')
-#model_classification = joblib.load('tag_classification.bkl')
-#tag_label = joblib.load('multilabel_binarizer.bkl')
+model_tfidf = joblib.load('tfidf_model.bkl')
+model_classification = joblib.load('tag_classification.bkl')
+indice = joblib.load('list_indice.bkl')
+title = joblib.load('list_title.bkl')
+docs = joblib.load('list_doc.bkl')
+tag_label = joblib.load('multilabel_binarizer.bkl')
+tf_idf_test = joblib.load('tfidf_test.bkl')
+true_tag = joblib.load('true_test.bkl')
 
 # Header of tag Prediction
-html_temp="""<div style="background-color:#F5F5F5"> <h1 style="color:#31333F;text-align:center;"> Customer Satisfaction Prediction </h1></div>"""
+html_temp="""<div style="background-color:#F5F5F5"> <h1 style="color:#31333F;text-align:center;">Tag Prediction </h1></div>"""
 
-st.text_input("Enter your text here", key="text")
+x = st.slider('x')
 
-# You can access the value at any point with:
-text = st.session_state.text
-
-text = cleanText(text)
-input_vector = model_tfidf.transform(text)        
+num =x
+ind = indice[num]
+title = title[ind]
+tags = true_tag[ind]
+input_vector = tf_idf_test[num]
 res = model_classification.predict(input_vector)
 res = tag_label.inverse_transform(res)
 
-st.success(f'The tag list is {res}')
+st.success(f'Document Title is {title}')
+st.success(f'Document Tags are {tags}')
+st.success(f'The predict's tags list is {res}')
