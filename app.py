27173a169bd6f3102755003168f39095858ee858
Emma Effa <eleffa@yahoo.com>
2023-07-19 14:47:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import joblib
 import numpy as np
 import re
 from bs4 import BeautifulSoup
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords    
 from nltk.stem.porter import PorterStemmer 
 
